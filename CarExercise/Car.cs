d6c1dab4b21713163cf3a7543a8ef0828b6b2b64
Keiti <keiti.nurmbek@gmail.com>
2021-02-25 17:02:27 +0200

    My Car Excercise

---
+++ CarExercise/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarExercise
+{
+    class Car //The car has a mark name (string), model name (string), registration number (string, no longer than six characters), color (string), odometer (int) and fuel tank (int)
+    {
+        private string _mark;
+        private string _model;
+        private string _registrationNr;
+        private string _color;
+        private int _odometer;
+        private int _fuelTank;
+
+        public Car(string mark, string model, string registrationNr, string color)
+        {
+            _mark = mark;
+            _model = model;
+            if (registrationNr.Length > 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(registrationNr), "Antud väärtus ei saa olla pikem kui 6 tähemärki.");
+            }
+            _registrationNr = registrationNr;
+            _color = color;
+            _odometer = 0;
+            _fuelTank = 60;
+        }
+
+        public string Mark
+        {
+            get
+            {
+                return _mark;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+        }
+        public string RegistrationNr
+        {
+            get
+            {
+                return _registrationNr;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+        }
+        public int Odometer
+        {
+            get
+            {
+                return _odometer;
+            }
+        }
+        public int FuelTank
+        {
+            get
+            {
+                return _fuelTank;
+            }
+        }
+
+        public void DriveCar()
+        {
+            _odometer += 100;
+            _fuelTank -= 5;
+        }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Mark: {_mark}\nModel: {_model}\nRegistration number: {_registrationNr}\nColor: {_color}\nOdometer: {_odometer}\nFuel tank: {_fuelTank}\n");
+        }
+    }
+}
---
+++ CarExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarExercise
+{
+    class Program
+    {
+        static void Main(string[] args) 
+
+        {
+            Car auto = new Car("Renault", "Clio", "291THB", "red");
+            auto.PrintCarInfo();
+
+            if (auto.FuelTank > 0)
+            {
+                while (auto.FuelTank > 0)
+                {
+                    auto.DriveCar();
+                    //auto.PrintCarInfo();//võib aktiveerida, et kontrollida toimimist
+                }
+                Console.WriteLine("The race is over!");
+                auto.PrintCarInfo();
+            }
+
+        }
+    }
+}
